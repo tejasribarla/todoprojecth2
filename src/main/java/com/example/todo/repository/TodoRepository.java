e0d614e47abd092e512a4a6f66bbaa467167b8ed
tejasribarla <tejasribarla188@gmail.com>
2023-03-11 13:45:13 +0000

    solution

---
+++ src/main/java/com/example/todo/repository/TodoRepository.java
@@ -0,0 +1,14 @@
+// Write your code here
+package com.example.todo.repository;
+
+import com.example.todo.model.Todo;
+
+import java.util.ArrayList;
+
+public interface TodoRepository {
+    ArrayList<Todo>getTodos();
+    Todo getTodoById(int id);
+    Todo addTodo(Todo todo);
+    Todo updateTodo(int id, Todo todo);
+    void deleteTodo(int id);
+}
